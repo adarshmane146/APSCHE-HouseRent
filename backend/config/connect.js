af2d90358c87d76fc9d39c22d1b7375a59ccce87
Yesebu <yesebumylapalli08@gmail.com>
2025-06-27 23:32:16 +0530

    Initial complete HouseRent project setup with frontend and backend

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectionOfDb = () => {
-  mongoose
-    .connect(process.env.MONGO_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-      throw new Error(`Could not connect to MongoDB: ${err}`);
-    });
-};
+const connectionofDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI); // no options here
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+}
 
-module.exports = connectionOfDb;
+module.exports = connectionofDb;
